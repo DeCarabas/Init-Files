de75eb18a646bbe1c355abfd4009a8831de224d5
John Doty <john@d0ty.me>
2014-11-09 15:04:21 -0800

    Fix the package path for MELPA.

--- .emacs.d/core.el
+++ .emacs.d/core.el
@@ -59,8 +59,11 @@
 (setq package-archives
       '(("gnu"         . "http://elpa.gnu.org/packages/")
         ("org"         . "http://orgmode.org/elpa/")
-        ("melpa"       . "http://melpa.milkbox.net/packages/")
+        ("melpa"       . "http://melpa.org/packages/")
         ("marmalade"   . "https://marmalade-repo.org/packages/")))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
 (defvar my-packages

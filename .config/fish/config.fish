0f714ed36e3f7a8b671b2abfd3261a7b35e55c2a
John Doty <john@d0ty.me>
2017-11-01 09:38:54 -0700

    Comment out broken stuff in config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,15 +2,15 @@ if status --is-login
     set PATH $PATH ~/bin ~/devtools/buck/bin
 end
 
-export EDITOR=ec
-export VISUAL=ec
+# export EDITOR=ec
+# export VISUAL=ec
 
-export FBANDROID_DIR=/Users/doty/fbsource/fbandroid
-alias quicklog_update=/Users/doty/fbsource/fbandroid/scripts/quicklog/quicklog_update.sh
-alias qlu=quicklog_update
+# export FBANDROID_DIR=/Users/doty/fbsource/fbandroid
+# alias quicklog_update=/Users/doty/fbsource/fbandroid/scripts/quicklog/quicklog_update.sh
+# alias qlu=quicklog_update
 
-# added by setup_fb4a.sh
-export ANDROID_SDK=/opt/android_sdk
-export ANDROID_NDK_REPOSITORY=/opt/android_ndk
-export ANDROID_HOME={ANDROID_SDK}
-export PATH={PATH}:{ANDROID_SDK}/tools:{ANDROID_SDK}/platform-tools
+# # added by setup_fb4a.sh
+# export ANDROID_SDK=/opt/android_sdk
+# export ANDROID_NDK_REPOSITORY=/opt/android_ndk
+# export ANDROID_HOME={ANDROID_SDK}
+# export PATH={PATH}:{ANDROID_SDK}/tools:{ANDROID_SDK}/platform-tools

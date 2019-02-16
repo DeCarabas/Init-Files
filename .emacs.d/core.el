13fa30ab22ab21a270e5f27db20d8666fc868530
John Doty <john@d0ty.me>
2019-02-16 08:20:47 -0800

    Fix env variables?

--- .emacs.d/core.el
+++ .emacs.d/core.el
@@ -786,68 +786,18 @@
 ;; Some build stuff; I swiped this from handmade-hero. Good for
 ;; unibuild setups.
 ;; =================================================================
-;; (when (featurep 'w32)
-;;   (setq doty-makescript "build.bat"))
+(defun set-vs-environment ()
+  "Load the VS environment variables into the current Emacs process."
+  (interactive)
+  (dolist
+      (ev (split-string
+           (shell-command-to-string "cmd /c \" \"%ProgramFiles(x86)%\\Microsoft Visual Studio\\2017\\Enterprise\\VC\\Auxiliary\\Build\\vcvarsall.bat\" x64 && set \"")
+           "[\n]+"))
+    (letrec ((spev (split-string ev "="))
+             (vn (car spev))
+             (vv (cadr spev)))
+      (setenv vn vv))))
 
-;; (when (featurep 'cocoa)
-;;   (setq doty-makescript "./build.macosx"))
-
-;; (when (featurep 'x)
-;;   (setq doty-makescript "./build.linux"))
-
-;; (setq compilation-directory-locked nil)
-;; (setq compilation-context-lines 0)
-;; (setq compilation-error-regexp-alist
-;;     (cons '("^\\([0-9]+>\\)?\\(\\(?:[a-zA-Z]:\\)?[^:(\t\n]+\\)(\\([0-9]+\\)) : \\(?:fatal error\\|warnin\\(g\\)\\) C[0-9]+:" 2 3 nil (4))
-;;      compilation-error-regexp-alist))
-
-;; (defun find-project-directory-recursive ()
-;;   "Recursively search for a makefile."
-;;   (interactive)
-;;   (if (file-exists-p doty-makescript) t
-;;       (cd "../")
-;;       (find-project-directory-recursive)))
-
-;; (defun lock-compilation-directory ()
-;;   "The compilation process should NOT hunt for a makefile."
-;;   (interactive)
-;;   (setq compilation-directory-locked t)
-;;   (message "Compilation directory is locked."))
-
-;; (defun unlock-compilation-directory ()
-;;   "The compilation process SHOULD hunt for a makefile."
-;;   (interactive)
-;;   (setq compilation-directory-locked nil)
-;;   (message "Compilation directory is roaming."))
-
-;; (defun find-project-directory ()
-;;   "Find the project directory."
-;;   (interactive)
-;;   (setq find-project-from-directory default-directory)
-;;   (switch-to-buffer-other-window "*compilation*")
-;;   (if compilation-directory-locked (cd last-compilation-directory)
-;;   (cd find-project-from-directory)
-;;   (find-project-directory-recursive)
-;;   (setq last-compilation-directory default-directory)))
-
-;; (defun make-without-asking ()
-;;   "Make the current build."
-;;   (interactive)
-;;   (if (find-project-directory) (compile doty-makescript))
-;;   (other-window 1))
-;; (define-key global-map "\C-c\C-m" 'make-without-asking)
-
-;; (defun set-vs-environment ()
-;;   "Load the VS environment variables into the current Emacs process."
-;;   (interactive)
-;;   (dolist
-;;       (ev (split-string
-;;            (shell-command-to-string "cmd /c \" \"%ProgramFiles(x86)%\\Microsoft Visual Studio 14.0\\VC\\vcvarsall.bat\" x64 && set \"")
-;;            "[\n]+"))
-;;     (letrec ((spev (split-string ev "="))
-;;              (vn (car spev))
-;;              (vv (cadr spev)))
-;;       (setenv vn vv))))
 
 
 ;; =================================================================

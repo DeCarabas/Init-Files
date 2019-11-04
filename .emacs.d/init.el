6e0948982683d642252b60725ebbb07a80cfffd8
John Doty <john@d0ty.me>
2019-11-04 15:23:54 -0800

    Some tweaks to emacs init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -264,6 +264,15 @@
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
 
+;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
+(defun unfill-paragraph (&optional region)
+  "Take a multi-line REGION and make it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+
 ;; =================================================================
 ;; Text mode configuration.
 ;; =================================================================

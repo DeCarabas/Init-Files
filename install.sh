14932b6656a61369775152704acfdd4e369c42c0
John Doty <john@d0ty.me>
2022-06-14 05:30:46 -0700

    Render that which belongs to caesar unto caesar

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#
+# This is my dotfiles setup script. It is invoked by coder.com instances.
+# We have this shared part of the setup, in setup.py, so let's run that first.
 MY_PATH=$(dirname "$0")
 cd $MY_PATH
+
 python3 "./setup.py"
+
+# OK this stuff here is better in bash, and also is specific to setting up
+# coder.com instances, so.
+sudo add-apt-repository ppa:kelleyk/emacs -y
+sudo apt-get update
+sudo apt-get install -y fish emacs27-nox tmux
+
+sudo chsh -s /usr/bin/fish $USER

836a118fb8567cd9d6ae1d15071dc651c837f07a
John Doty <john@d0ty.me>
2022-06-14 18:33:46 -0700

    More stuff

--- install.sh
+++ install.sh
@@ -8,9 +8,21 @@ cd $MY_PATH
 python3 "./setup.py"
 
 # OK this stuff here is better in bash, and also is specific to setting up
-# coder.com instances, so.
+# coder.com instances, so. Add packages that I want in my coder image.
 sudo add-apt-repository ppa:kelleyk/emacs -y
 sudo apt-get update
 sudo apt-get install -y fish emacs27-nox tmux
 
+# NOTE: THIS NEEDS TO BE INSTALLED IN THE BASE IMAGE
+sudo apt-get install -y libtinfo5
+
+# Change my shell to fish.
 sudo chsh -s /usr/bin/fish $USER
+
+# Make sure that the gitconfig that's on my computer links to my shared
+# config.
+git config --global include.path .gitconfig.shared
+
+# Install local rust
+# (This should be in the image I think.)
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y

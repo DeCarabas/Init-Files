466bc0bb58492799d2e98e11cb21a3f627efaecc
John Doty <john@d0ty.me>
2024-11-17 08:43:24 -0800

    [emacs] Whoops fix that!

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -364,7 +364,7 @@
   "Doty's hook for text mode."
   (setq fill-column 70)
   (turn-on-auto-fill)
-  (my--fix-aspell)
+  (my/fix-aspell)
   (flyspell-mode))
 
 (add-hook 'text-mode-hook 'my-text-mode-hook)
@@ -950,7 +950,7 @@ Or, uh, Objective C, I guess."
 (defun my-org-mode-hook ()
   "My org mode hook."
   (turn-off-filladapt-mode)
-  (my--fix-aspell)
+  (my/fix-aspell)
   (require 'ox-quip))
 
 (use-package org
@@ -1322,5 +1322,31 @@ Or, uh, Objective C, I guess."
 ;; =================================================================
 (use-package swift-mode :ensure t
   :mode "\\.swift\\(interface\\)?\\'")
+
+;; =================================================================
+;; Combobulate
+;; =================================================================
+(use-package combobulate
+  :preface
+  ;; You can customize Combobulate's key prefix here.
+  ;; Note that you may have to restart Emacs for this to take effect!
+  (setq combobulate-key-prefix "C-c o")
+
+  ;; Optional, but recommended.
+  ;;
+  ;; You can manually enable Combobulate with `M-x
+  ;; combobulate-mode'.
+  ;; :hook
+  ;; ((python-ts-mode . combobulate-mode)
+  ;;  (js-ts-mode . combobulate-mode)
+  ;;  (html-ts-mode . combobulate-mode)
+  ;;  (css-ts-mode . combobulate-mode)
+  ;;  (yaml-ts-mode . combobulate-mode)
+  ;;  (typescript-ts-mode . combobulate-mode)
+  ;;  (json-ts-mode . combobulate-mode)
+  ;;  (tsx-ts-mode . combobulate-mode))
+  ;; Amend this to the directory where you keep Combobulate's source
+  ;; code.
+  :load-path ("/home/doty/src/combobulate"))
 
 ;;; init.el ends here

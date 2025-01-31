a5ed54249393d203d82cfc45f3c6754ef09b73e1
John Doty <john@d0ty.me>
2025-01-31 04:28:36 +0000

    [emacs] No, not combolate

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1326,27 +1326,27 @@ Or, uh, Objective C, I guess."
 ;; =================================================================
 ;; Combobulate
 ;; =================================================================
-(use-package combobulate
-  :preface
-  ;; You can customize Combobulate's key prefix here.
-  ;; Note that you may have to restart Emacs for this to take effect!
-  (setq combobulate-key-prefix "C-c o")
+;; (use-package combobulate
+;;   :preface
+;;   ;; You can customize Combobulate's key prefix here.
+;;   ;; Note that you may have to restart Emacs for this to take effect!
+;;   (setq combobulate-key-prefix "C-c o")
 
-  ;; Optional, but recommended.
-  ;;
-  ;; You can manually enable Combobulate with `M-x
-  ;; combobulate-mode'.
-  ;; :hook
-  ;; ((python-ts-mode . combobulate-mode)
-  ;;  (js-ts-mode . combobulate-mode)
-  ;;  (html-ts-mode . combobulate-mode)
-  ;;  (css-ts-mode . combobulate-mode)
-  ;;  (yaml-ts-mode . combobulate-mode)
-  ;;  (typescript-ts-mode . combobulate-mode)
-  ;;  (json-ts-mode . combobulate-mode)
-  ;;  (tsx-ts-mode . combobulate-mode))
-  ;; Amend this to the directory where you keep Combobulate's source
-  ;; code.
-  :load-path ("/home/doty/src/combobulate"))
+;;   ;; Optional, but recommended.
+;;   ;;
+;;   ;; You can manually enable Combobulate with `M-x
+;;   ;; combobulate-mode'.
+;;   ;; :hook
+;;   ;; ((python-ts-mode . combobulate-mode)
+;;   ;;  (js-ts-mode . combobulate-mode)
+;;   ;;  (html-ts-mode . combobulate-mode)
+;;   ;;  (css-ts-mode . combobulate-mode)
+;;   ;;  (yaml-ts-mode . combobulate-mode)
+;;   ;;  (typescript-ts-mode . combobulate-mode)
+;;   ;;  (json-ts-mode . combobulate-mode)
+;;   ;;  (tsx-ts-mode . combobulate-mode))
+;;   ;; Amend this to the directory where you keep Combobulate's source
+;;   ;; code.
+;;   :load-path ("/home/doty/src/combobulate"))
 
 ;;; init.el ends here

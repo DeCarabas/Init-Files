e5b297962a221c7fe13bd2d4b683b349fd5fc754
John Doty <john.doty@databricks.com>
2025-05-13 19:45:37 +0000

    [emacs] Keywords as italics is nice

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -119,3 +119,9 @@
  '(which-func-mode-global t nil (which-func))
  '(widget-editable-list-gui t)
  '(x-stretch-cursor nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-keyword-face ((t (:slant italic)))))

f9e570eb67a2f4b5da526c3dad9068db2cad804b
John Doty <john@d0ty.me>
2026-01-31 18:31:51 +0000

    [emacs] Better eat

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1508,6 +1508,15 @@ Do this when you edit your project view."
 ;; =================================================================
 ;; Terminal
 ;; =================================================================
-(use-package eat :straight t)
-
+;; (use-package eat :straight t)
+(use-package eat
+  :straight '(eat :type git
+                  :host codeberg
+                  :repo "akib/emacs-eat"
+                  :files ("*.el" ("term" "term/*.el") "*.texi"
+                          "*.ti" ("terminfo/e" "terminfo/e/*")
+                          ("terminfo/65" "terminfo/65/*")
+                          ("integration" "integration/*")
+                          (:exclude ".dir-locals.el" "*-tests.el")))
+    :bind (("C-c T" . eat-project)))
 ;;; init.el ends here
